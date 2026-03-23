d8493f8e12ad224d7a945dce2bf21ef4cace4fbc
pirhanow <gutmoodx@gmail.com>
2026-03-24 01:50:25 +0300

    Chanched controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function show ($id)
+    {
+        return view ('about', ['name' => 'User '.$id]);
+    }
+
+    public function profile () {
+return 'Profile page';
+    }
 }
