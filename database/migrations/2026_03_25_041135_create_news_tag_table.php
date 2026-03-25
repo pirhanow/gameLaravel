57677f1b451a58ce3a55347627d4bf1f4eb0223d
pirhanow <gutmoodx@gmail.com>
2026-03-25 08:36:50 +0300

    feat: create tabele news, categories, tags, news_tag

---
+++ database/migrations/2026_03_25_041135_create_news_tag_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('news_id')->constrained('tags')->onDelete('cascade');
+            $table->foreignID('tag_id')->constrained('tags')->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news_tag');
+    }
+};
