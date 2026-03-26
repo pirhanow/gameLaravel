622f9df853b03129215ffb7f5bc841ed06b0e6bb
pirhanow <gutmoodx@gmail.com>
2026-03-26 23:23:07 +0300

    feat: implement Eloquent models and database structure

---
+++ app/Models/Category.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'slug', 'description'];
+public function news() {
+                return $this->hasMany(News::class);
+            }
+    }
